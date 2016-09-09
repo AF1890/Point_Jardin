065a6c8025c0d6877737aa822f9610565a8709e0
AF1890 <aylin.devillechenous@gmail.com>
2016-09-09 11:05:45 +0200

    modif2

--- footer.php
+++ footer.php
@@ -1,1 +1,19 @@
-idyfy
+
+<p>test footer
+</p>
+
+<!-- appel script-->
+<script
+	src="http://code.jquery.com/jquery-2.2.4.js"
+	integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="
+	crossorigin="anonymous">
+</script>
+		
+ 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js">
+</script>
+
+
+
+</body>
+</html>
