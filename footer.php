967e0bdba227dc45d289569c54eab9241c92c89e
AF1890 <aylin.devillechenous@gmail.com>
2016-09-09 13:27:21 +0200

    modif index et footer

--- footer.php
+++ footer.php
@@ -1,19 +1,47 @@
-
-<p>test footer
-</p>
-
-<!-- appel script-->
-<script
-	src="http://code.jquery.com/jquery-2.2.4.js"
-	integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="
-	crossorigin="anonymous">
-</script>
-		
- 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js">
-</script>
+<footer class="page-footer teal">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Point Jardin</h5>
+          <p class="grey-text text-lighten-4">mini descriptif </p>
 
 
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Settings</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Connect</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    
+	
+	
+	<div class="footer-copyright">
+      <div class="container">
+      2015 Free Style All Rights Reserved | Design by W3layouts 
+      </div>
+    </div>
+  </footer>
 
-</body>
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
 </html>
