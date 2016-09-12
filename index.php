8b3435c757e632aaeb710d7b1b87d49918566228
AF1890 <aylin.devillechenous@gmail.com>
2016-09-12 16:07:42 +0200

    modif index

--- index.php
+++ index.php
@@ -310,14 +310,4 @@
 
 
 
-  <!--  Scripts-->
-  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="js/materialize.js"></script>
-  <script src="js/init.js"></script>
-
-
-
 <?php include('footer.php'); ?>
-
-  </body>
-</html>
