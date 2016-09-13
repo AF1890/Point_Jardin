f66f3a23fd003e4dea6a0fdedba5aab961f7b67b
bruno-bouvet <bouvet.bruno@gmail.com>
2016-09-13 15:18:31 +0200

    nouveaux header et creation branche

---
+++ header.php
@@ -0,0 +1,103 @@
+<header class="collapsible-header">
+        <div class="row blue-grey darken-3">
+            <div class="col s7 testcls">
+          
+            </div>
+            <div class="connexion col s5 testcls left"><a href="#!">Me connecter</a> ou <a href="#!">créer un compte</a> </div>            
+        </div>
+       
+        <!-- header middle  -->
+      
+        <div class="row">
+            <div class="col s3 testcls2 valign-wrapper">
+               <div class ="taille_logo"> 
+                <img src="images\logo_jardin_small.png" alt="Logo Point Jardin">
+            </div>
+            </div>
+            <div class="col s3 testcls2">
+                <form>
+					<div class="inputfield2 input-field">
+					  <input id="search" type="search" required>
+					  <label for="search"><i class="material-icons">search</i></label>
+					  <i class="material-icons">close</i>
+					</div>
+				</form>
+            </div>
+            <div class="col s1 testcls2"></div>
+        </div>           
+        </header>
+	
+
+<!-- Dropdown Structure -->
+<ul id="MINERAL" class="dropdown-content">
+  <li><a href="#!">Graviers décoratifs</a></li>
+  <li><a href="#!">Galets</a></li>
+  <li><a href="#!">Ardoise</a></li>
+    <li><a href="#!">Garden Stone</a></li>
+    <li><a href="#!">La Brique</a></li>
+    <li><a href="#!">Pouzzolane</a></li>
+    <li><a href="#!">Gros volumes</a></li>
+    <li><a href="#!">Le schiste</a></li>
+    <li><a href="#!">Le sable</a></li>
+    <li><a href="#!">Opus Incertum</a></li>
+</ul>
+<ul id="VEGETAL" class="dropdown-content">
+  <li><a href="#!">Ecorce de Pin</a></li>
+  <li><a href="#!">Plaquettes Peuplier</a></li>
+  <li><a href="#!">Terreau</a></li>
+    <li><a href="#!">Terre Végétale</a></li>
+    <li><a href="#!">Terre de Bruyère</a></li>
+    <li><a href="#!">Chanvre</a></li>
+    <li><a href="#!">Noyaux de Fruits</a></li>
+
+</ul>
+<ul id="NOS_SERVICES" class="dropdown-content">
+  <li><a href="#!">Bordures</a></li>
+  <li><a href="#!">Engrais</a></li>
+  <li><a href="#!">Gazon de placage</a></li>
+    <li><a href="#!">Traverse</a></li>
+    <li><a href="#!">Gazon</a></li>
+    <li><a href="#!">Dalles alvéolées</a></li>
+    
+</ul>
+<ul id="NOS_SERVICES" class="dropdown-content">
+  <li><a href="#!">Bordures</a></li>
+  <li><a href="#!">Engrais</a></li>
+  <li><a href="#!">Gazon de placage</a></li>
+    <li><a href="#!">Traverse</a></li>
+    <li><a href="#!">Gazon</a></li>
+    <li><a href="#!">Dalles alvéolées</a></li>
+    
+</ul>
+<ul id="QUI_SOMMES_NOUS" class="dropdown-content">
+  <li><a href="#!">one</a></li>
+  <li><a href="#!">two</a></li>
+  <li class="divider"></li>
+  <li><a href="#!">three</a></li>
+</ul>
+    
+<nav class="white" role="navigation">
+  <div class=" second_nav nav-wrapper">
+    <ul class="hide-on-med-and-down">
+     <!-- <li><a href="index.php">Accueil</a></li>
+      <li><a href="badges.html">Components</a></li>-->
+        
+      <!-- Dropdown Trigger -->
+        <li><a href="index.php">ACCUEIL</a></li>-->
+        
+        <li><a class="dropdown-button" href="#!" data-activates="MINERAL">MINERAL<i 
+            class="material-icons right">arrow_drop_down</i></a></li>
+        
+        <li><a class="dropdown-button" href="#!" data-activates="VEGETAL">VEGETAL<i 
+            class="material-icons right">arrow_drop_down</i></a></li>
+
+        <li><a class="dropdown-button" href="#!" data-activates="ACCESSOIRES">ACCESSOIRES<i class="material-icons right">arrow_drop_down</i></a></li>
+        
+        <li><a class="dropdown-button" href="#!" data-activates="NOS_SERVICES">NOS SERVICES<i class="material-icons right">arrow_drop_down</i></a></li>
+        
+        <li><a class="dropdown-button" href="#!" data-activates="QUI_SOMMES_NOUS">QUI SOMMES NOUS ?<i class="material-icons right">arrow_drop_down</i></a></li>
+        
+    </ul>
+  </div>
+</nav>
+            
