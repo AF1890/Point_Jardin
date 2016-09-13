2672a384d445a8929f8a8a3bafce9584fcda469c
AF1890 <aylin.devillechenous@gmail.com>
2016-09-13 14:47:40 +0200

    modif footer.css et footer.php et ajout sprite.png

--- footer.php
+++ footer.php
@@ -1,42 +1,60 @@
-<footer class="page-footer teal">
+<footer class="page-footer">
     <div class="container">
       <div class="row">
-        <div class="contact col l6 s12">
+        <div class="contact col l3 s12">
+          <h5 class="white-text">A propos de Point jardin</h5>
+           <ul>
+            <li><a class="white-text" href="#!">Qui-sommes nous ?</a></li>
+            <li><a class="white-text" href="#!">Toutes les marques</a></li>
+            <li><a class="white-text" href="#!">Conditions générales de vente</a></li>
+            <li><a class="white-text" href="#!">Mentions légales</a></li>
+            <li><a class="white-text" href="#!">Protection des données</a></li>
+          </ul>
+        </div>       
+
+        <div class="info col l3 s12">
+          <h5 class="white-text">Pour les particuliers</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Informations livraison</a></li>
+            <li><a class="white-text" href="#!">Suivre votre commande</a></li>
+            <li><a class="white-text" href="#!">Faire un retour</a></li>
+          </ul>
+          <h5 class="white-text">Pour les professionnels</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Ventes aux professionnels</a></li>
+            <li><a class="white-text" href="#!">Fournisseurs</a></li>
+            <li><a class="white-text" href="#!">Toutes demandes professionnelles</a></li>
+          </ul>
+        </div>
+
+        <div class="suivre col l3 s12">
+          <h5 class="white-text">Nous suivre</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+          </ul>
+        </div>
+
+
+        <div class="contact col l3 s12">
           <h5 class="white-text">Nous contacter</h5>
           <p class="grey-text text-lighten-4">par téléphone <br /> 
           <span style="font-size: 20px;">04 86 13 91 10</span> <br /> 
           Notre service client est là pour vous répondre <br /> du Lundi au Samedi de 9h à 12h30 et de 14h à 19h.</p>
         </div>
 
-        <div class="suivre col l3 s12">
-         <div id="icons">
-          <h5 class="white-text">Nous suivre</h5>
-          <ul>
-            <li><a class="white-text" href= "https://fr-fr.facebook.com/PointJardin/"><img src="http://i.imgur.com/iueNI4c.png">Facebook</a></li>
-            <li><a class="white-text" href="#!">Link 2</a></li>
-            <li><a class="white-text" href="#!">Link 3</a></li>
-            <li><a class="white-text" href="#!">Link 4</a></li>
-          </ul>
-          </div>
-        </div>
-
-        <div class="info col l3 s12">
-          <h5 class="white-text">Informations livraison</h5>
-          <ul>
-            <li><a class="white-text" href="#!">Link 1</a></li>
-            <li><a class="white-text" href="#!">Link 2</a></li>
-            <li><a class="white-text" href="#!">Link 3</a></li>
-            <li><a class="white-text" href="#!">Link 4</a></li>
-          </ul>
-        </div>
       </div>
     </div>
     
-	
-	
+	 <div class="footer-herb">
+          <div class="image col s12">
+            <img src="sprite.png">
+          </div>
+    </div>
+
 	<div class="footer-copyright">
       <div class="container">
-      &copy; 2016 All Rights Reserved | Mentions légales
+      &copy; 2016 All Rights Reserved Point Jardin | Design by WCS | Mentions légales
       </div>
     </div>
   </footer>
