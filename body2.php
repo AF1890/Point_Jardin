3b8f66af5356d1e912154c4742e83a2c0efb533e
AF1890 <aylin.devillechenous@gmail.com>
2016-09-15 17:22:09 +0200

    modfi body2

--- body2.php
+++ body2.php
@@ -1,18 +1,91 @@
+
 <div class="container">
-      <div class="row">
-        <div class="video col l8 s12">
-	      <div class="video-container">
-	        <iframe width="560" height="315" src="https://www.youtube.com/embed/szGkJgvt2-0" frameborder="0" allowfullscreen></iframe>
-	      </div>
+    <div class="row" id="body2">
+        <div class="col l4 s12" id="video">
+            <iframe width="360" height="315" src="https://www.youtube.com/embed/szGkJgvt2-0" frameborder="0" allowfullscreen></iframe>
         </div>
-      </div>
 
+        <div class="col l4 s12" id="bienvenue">
+            <h5><span style="font-size:27px;">Bienvenue chez Point Jardin</span></h5>
+            <h5>Nos services</h5>
+            <div class ="responsive-table">
+                <tr>
+                    <td><img src="https://s17.postimg.org/i1o3ef267/unnamed.png" alt="icone1" height=55px width="55px" />"</td>
+                    <td>Achetez la quantité dont vous avez besoin</td>
+                </tr>
+                <tr>
+                    <td><img src="https://s17.postimg.org/d6bz3pgn3/icone2.png" alt="icone2" height=55px width="55px" /></td>
+                    <td>En panne d’inspiration? Nous vous conseillons et dessinons votre jardin</td>
+                <br /></tr>
+                <tr>
+                    <td><img class="displayed" src="https://s11.postimg.org/4tl2rqdfn/icone3.png" alt="icone3" height=55px width="55px"/>"</td>
+                    <td>Embellisez votre jardin avec notre gamme de paillage variés</td>
+                <br /></tr>
+                <tr>
+                    <td><img class="displayed" src="https://s17.postimg.org/d6bz3pgn3/icone2.png" alt="icone4" height=55px width="55px"/>"</td>
+                    <td>Pas de véhicule adapté? Point jardin vous livre chez vous, petite ou grande quantité</td>
+                </tr>
+            </div>
 
-      <div class="row">
-        <div class="form col l4 s12">
-	      <div class="video-container">
-	        <p>test</p>
-	      </div>
         </div>
-      </div>
+
+        <div class="col l3 s12" id="form">
+            <form> 
+                <h5>Demandez votre devis</h5>
+                    <div class="row">
+                        <div class="input-field col s6">
+                            <input placeholder="Prénom" id="fPrénom" type="text" class="validate">
+                            <label for="Prénom">Prénom</label>
+                        </div>
+                        <div class="input-field col s6">
+                            <input id="Nom" type="text" class="validate">
+                            <label for="last_name">Nom</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input id="email" type="email" class="validate">
+                            <label for="email">Email</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input id="commentairee" type="commentairee" class="validate">
+                            <label for="commentairee">commentaire</label>
+                        </div>
+                    </div>
+            </form>
+        </div>
+    </div>
 </div>
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--flexbox
+
+<div id="container">
+<div id="left-box">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/szGkJgvt2-0" frameborder="0" allowfullscreen></iframe>
+</div>
+
+<div id="right-box">
+<div id="form">
+<h2>Mes expériences</h2>
+</div>
+</div>
+</div>
+-->
