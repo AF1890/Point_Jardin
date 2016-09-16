048056621a800d34ef8e3d98e3b4934ad40cb327
AF1890 <aylin.devillechenous@gmail.com>
2016-09-16 10:58:59 +0200

    modif medai query

--- body2.php
+++ body2.php
@@ -1,35 +1,41 @@
 
 <div class="container">
     <div class="row" id="body2">
-        <div class="col l4 s12" id="video">
+
+<!-- VIDEO --> 
+        <div class="col l4 s12 larger-block" id="video">
             <iframe width="360" height="315" src="https://www.youtube.com/embed/szGkJgvt2-0" frameborder="0" allowfullscreen></iframe>
         </div>
 
-        <div class="col l4 s12" id="bienvenue">
+
+<!-- BIENVENUE --> 
+        <div class="col l4 s12 larger-block" id="bienvenue">
             <h5><span style="font-size:27px;">Bienvenue chez Point Jardin</span></h5>
             <h5>Nos services</h5>
             <div class ="responsive-table">
                 <tr>
-                    <td><img src="https://s17.postimg.org/i1o3ef267/unnamed.png" alt="icone1"  width="55px" /></td>
+                    <td><img src="https://s17.postimg.org/i1o3ef267/unnamed.png" alt="icone1"  width="50px" /></td>
                     <td>Achetez la quantité dont vous avez besoin</td>
                 </tr>
                 <tr>
-                    <td><img src="https://s17.postimg.org/d6bz3pgn3/icone2.png" alt="icone2" height=55px width="55px" /></td>
+                    <td><img src="https://s17.postimg.org/d6bz3pgn3/icone2.png" alt="icone2" width="50px" /></td>
                     <td>En panne d’inspiration? Nous vous conseillons et dessinons votre jardin</td>
                 <br /></tr>
                 <tr>
-                    <td><img class="displayed" src="https://s11.postimg.org/4tl2rqdfn/icone3.png" alt="icone3" height=55px width="55px"/>"</td>
+                    <td><img class="displayed" src="https://s11.postimg.org/4tl2rqdfn/icone3.png" alt="icone3" width="50px"/>"</td>
                     <td>Embellisez votre jardin avec notre gamme de paillage variés</td>
                 <br /></tr>
                 <tr>
-                    <td><img class="displayed" src="https://s17.postimg.org/d6bz3pgn3/icone2.png" alt="icone4" height=55px width="55px"/>"</td>
+                    <td><img class="displayed" src="https://s17.postimg.org/d6bz3pgn3/icone2.png" alt="icone4" width="50px"/>"</td>
                     <td>Pas de véhicule adapté? Point jardin vous livre chez vous, petite ou grande quantité</td>
                 </tr>
             </div>
 
         </div>
 
-        <div class="col l3 s12" id="form">
+
+<!-- FORMULAIRE --> 
+        <div class="col l3 s12 larger block" id="form">
             <form> 
                 <h5>Demandez votre devis</h5>
                     <div class="row">
@@ -59,33 +65,3 @@
     </div>
 </div>
  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!--flexbox
-
-<div id="container">
-<div id="left-box">
-<iframe width="560" height="315" src="https://www.youtube.com/embed/szGkJgvt2-0" frameborder="0" allowfullscreen></iframe>
-</div>
-
-<div id="right-box">
-<div id="form">
-<h2>Mes expériences</h2>
-</div>
-</div>
-</div>
--->
