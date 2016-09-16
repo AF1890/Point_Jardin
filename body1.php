8d5d9f74fbed837620968ea23477c5bd9e3cb481
AF1890 <aylin.devillechenous@gmail.com>
2016-09-16 13:40:35 +0200

    modif

--- body1.php
+++ body1.php
@@ -1,41 +1,44 @@
-<div class="container">
-    <div class="row" id="body1">
+    <div class="vert">
+<div class="container" id="container1">
 
-<!-- SLIDER --> 
-<div class="col l4 s12 larger-block" id="slider">
-        <div class="slider">
-            <ul class="slides">
-                <li> <img class="responsive-img" src="images/cage.jpg">
-                    <div class="caption center-align">
-                        <h3>Grand choix de cages </h3>
-                        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5> </div>
-                </li>
-                <li> <img class="responsive-img" src="images/galet.jpg">
-                    <div class="caption left-align">
-                        <h3>Left Aligned Caption</h3>
-                        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5> </div>
-                </li>
-                <li> <img class="responsive-img" src="images/pierre.jpg">
-                    <div class="caption right-align">
-                        <h3>Un grand choix de pierres</h3>
-                        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5> </div>
-                </li>
-                <li> <img class="responsive-img" src="images/gravat.jpg">
-                    <div class="caption center-align">
-                        <h3>This is our big Tagline!</h3>
-                        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5> </div>
-                </li>
-                <li> <img class="responsive-img" src="images/gazon.png">
-                    <div class="caption center-align">
-                        <h3>This is our big Tagline!</h3>
-                        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5> </div>
-                </li>
-            </ul>
-        </div>
-</div>
-<!-- fin slider  -->
+        <div class="row" id="body1">
 
-</div>
+    <!-- SLIDER --> 
+    <div class="col l8 s12" id="slider">
+            <div class="slider">
+                <ul class="slides">
+                    <li> <img class="responsive-img" src="images/cage.jpg">
+                        <div class="caption center-align">
+                            <h3>Grand choix de cages </h3>
+                            <h5 class="light grey-text text-lighten-3">Nouveautés !</h5> </div>
+                    </li>
+                    <li> <img class="responsive-img" src="images/galet.jpg">
+                        <div class="caption left-align">
+                            <h3>Left Aligned Caption</h3>
+                            <h5 class="light grey-text text-lighten-3">Nouveautés !</h5> </div>
+                    </li>
+                    <li> <img class="responsive-img" src="images/pierre.jpg">
+                        <div class="caption right-align">
+                            <h3>Un grand choix de pierres</h3>
+                            <h5 class="light grey-text text-lighten-3">Nouveautés !</h5> </div>
+                    </li>
+                    <li> <img class="responsive-img" src="images/gravat.jpg">
+                        <div class="caption center-align">
+                            <h3>This is our big Tagline!</h3>
+                            <h5 class="light grey-text text-lighten-3">Nouveautés !</h5> </div>
+                    </li>
+                    <li> <img class="responsive-img" src="images/gazon.png">
+                        <div class="caption center-align">
+                            <h3>This is our big Tagline!</h3>
+                            <h5 class="light grey-text text-lighten-3">Nouveautés !</h5> </div>
+                    </li>
+                </ul>
+            </div>
+    </div>
+    <!-- fin slider  -->
+
+    </div>
+    </div>
 </div>
 
 <!-- script--> 
