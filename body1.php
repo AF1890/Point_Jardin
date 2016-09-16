683deff2c4d3b63b42e2eca81d7e0689d8b2fdbd
AF1890 <aylin.devillechenous@gmail.com>
2016-09-16 11:36:02 +0200

    verfi tout

--- body1.php
+++ body1.php
@@ -4,62 +4,39 @@
 <!-- SLIDER --> 
 <div class="col l4 s12 larger-block" id="slider">
         <div class="slider">
-    <ul class="slides">
-        <li> <img class="responsive-img" src="images/cage.jpg">
-            <div class="caption center-align">
-                <h3>Grand choix de cages </h3>
-                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5> </div>
-        </li>
-        <li> <img class="responsive-img" src="images/galet.jpg">
-            <div class="caption left-align">
-                <h3>Left Aligned Caption</h3>
-                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5> </div>
-        </li>
-        <li> <img class="responsive-img" src="images/pierre.jpg">
-            <div class="caption right-align">
-                <h3>Un grand choix de pierres</h3>
-                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5> </div>
-        </li>
-        <li> <img class="responsive-img" src="images/gravat.jpg">
-            <div class="caption center-align">
-                <h3>This is our big Tagline!</h3>
-                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5> </div>
-        </li>
-        <li> <img class="responsive-img" src="images/gazon.png">
-            <div class="caption center-align">
-                <h3>This is our big Tagline!</h3>
-                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5> </div>
-        </li>
-    </ul>
-</div>
+            <ul class="slides">
+                <li> <img class="responsive-img" src="images/cage.jpg">
+                    <div class="caption center-align">
+                        <h3>Grand choix de cages </h3>
+                        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5> </div>
+                </li>
+                <li> <img class="responsive-img" src="images/galet.jpg">
+                    <div class="caption left-align">
+                        <h3>Left Aligned Caption</h3>
+                        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5> </div>
+                </li>
+                <li> <img class="responsive-img" src="images/pierre.jpg">
+                    <div class="caption right-align">
+                        <h3>Un grand choix de pierres</h3>
+                        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5> </div>
+                </li>
+                <li> <img class="responsive-img" src="images/gravat.jpg">
+                    <div class="caption center-align">
+                        <h3>This is our big Tagline!</h3>
+                        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5> </div>
+                </li>
+                <li> <img class="responsive-img" src="images/gazon.png">
+                    <div class="caption center-align">
+                        <h3>This is our big Tagline!</h3>
+                        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5> </div>
+                </li>
+            </ul>
+        </div>
 </div>
 <!-- fin slider  -->
 
-
-<!-- FACEBOOK --> 
-        <div class="col l4 s12 larger-block" id="facebook">
-            <h5><span style="font-size:27px;">Bienvenue chez Point Jardin</span></h5>
-            <h5>Nos services</h5>
-            <div class ="responsive-table">
-                <tr>
-                    <td><img src="https://s17.postimg.org/i1o3ef267/unnamed.png" alt="icone1"  width="50px" /></td>
-                    <td>Achetez la quantité dont vous avez besoin</td>
-                </tr>
-                <tr>
-                    <td><img src="https://s17.postimg.org/d6bz3pgn3/icone2.png" alt="icone2" width="50px" /></td>
-                    <td>En panne d’inspiration? Nous vous conseillons et dessinons votre jardin</td>
-                <br /></tr>
-                <tr>
-                    <td><img class="displayed" src="https://s11.postimg.org/4tl2rqdfn/icone3.png" alt="icone3" width="50px"/>"</td>
-                    <td>Embellisez votre jardin avec notre gamme de paillage variés</td>
-                <br /></tr>
-                <tr>
-                    <td><img class="displayed" src="https://s17.postimg.org/d6bz3pgn3/icone2.png" alt="icone4" width="50px"/>"</td>
-                    <td>Pas de véhicule adapté? Point jardin vous livre chez vous, petite ou grande quantité</td>
-                </tr>
-            </div>
-
-        </div>
+</div>
+</div>
 
 <!-- script--> 
 
